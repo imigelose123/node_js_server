2ad47ca8ddc3383423f430805857e4b08d62ac1d
imigelose123 <101578354+imigelose123@users.noreply.github.com>
2023-04-08 20:35:32 -0400

    segundo commit

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ app.get('/op', (req, res) => {
     res.send('hola op')
   })
 
+  app.get('/op2', (req, res) => {
+    res.send('hola op2')
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
